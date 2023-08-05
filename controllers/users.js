01c0b3ecde694b02fa59559e12cfd456cb272608
Julia <july912655@gmail.com>
2023-08-05 07:25:14 +0500

    13пр user

--- controllers/users.js
+++ controllers/users.js
@@ -13,20 +13,19 @@ module.exports.getUsers = (req, res) => {
 
 module.exports.getUserById = (req, res) => {
   const { userId } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(userId)) {
-    res.status(400).send({ message: 'Некорректный ID пользователя' });
-    return;
-  }
   User.findById(userId)
+    .orFail()
     .then((user) => {
-      if (!user) {
-        res.status(404).send({ message: 'Пользователь не найден' });
-        return;
-      }
       res.send(user);
     })
-    .catch(() => {
-      res.status(500).send({ message: 'На сервере произошла ошибка' });
+    .catch((err) => {
+      if (!mongoose.Types.ObjectId.isValid(userId)) {
+        res.status(400).send({ message: 'Некорректный ID пользователя' });
+      } else if (err instanceof mongoose.Error.DocumentNotFoundError) {
+        res.status(404).send({ message: 'Пользователь не найден' });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
     });
 };
 
@@ -37,7 +36,7 @@ module.exports.createUser = (req, res) => {
       res.status(201).send(user);
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err instanceof mongoose.Error.ValidationError) {
         res.status(400).send({ message: err.message });
       } else {
         res.status(500).send({ message: 'На сервере произошла ошибка' });
@@ -47,25 +46,20 @@ module.exports.createUser = (req, res) => {
 
 module.exports.updateProfile = (req, res) => {
   const { name, about } = req.body;
-  // if (!name || !about) {
-  //   res.status(400).send({ message: 'Необходимо заполнить все поля' });
-  //   return;
-  // }
   User.findByIdAndUpdate(
     req.user._id,
     { name, about },
     { new: true, runValidators: true },
   )
+    .orFail()
     .then((updatedUser) => {
-      if (!updatedUser) {
-        res.status(404).send({ message: 'Пользователь не найден' });
-        return;
-      }
       res.send(updatedUser);
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err instanceof mongoose.Error.ValidationError) {
         res.status(400).send({ message: err.message });
+      } else if (err instanceof mongoose.Error.DocumentNotFoundError) {
+        res.status(404).send({ message: 'Пользователь не найден' });
       } else {
         res.status(500).send({ message: 'На сервере произошла ошибка' });
       }
@@ -79,16 +73,15 @@ module.exports.updateAvatar = (req, res) => {
     { avatar },
     { new: true, runValidators: true },
   )
+    .orFail()
     .then((updatedUser) => {
-      if (!updatedUser) {
-        res.status(404).send({ message: 'Пользователь не найден' });
-        return;
-      }
       res.send(updatedUser);
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err instanceof mongoose.Error.ValidationError) {
         res.status(400).send({ message: err.message });
+      } else if (err instanceof mongoose.Error.DocumentNotFoundError) {
+        res.status(404).send({ message: 'Пользователь не найден' });
       } else {
         res.status(500).send({ message: 'На сервере произошла ошибка' });
       }
