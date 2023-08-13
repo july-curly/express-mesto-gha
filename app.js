5fab94bbc2e44232c0e8dc9de3f482a43f33c7f7
Julia <july912655@gmail.com>
2023-08-13 19:01:05 +0500

    14 пр 5

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-const { handleError } = require('./middlewares/handleError');
+const handleError = require('./middlewares/handleError');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
